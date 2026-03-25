378d4329a71fdf7340573b7f260827f9c1d770ae
mking <mkinggrafix@gmail.com>
2026-03-25 16:44:09 -0700

    commit changes

---
+++ src/features/profile/components/Navbar.jsx
@@ -0,0 +1,21 @@
+const Navbar = () => {
+    <div>
+        <div>
+            DataEre
+        </div>
+
+        <div>
+
+        </div>
+
+        <div>
+            <button>Upgrade</button>
+        </div>
+
+        <div>
+
+        </div>
+    </div>
+}
+
+export default Navbar;
